af12acc56f0fcf098ad330ea57b9e21afc1a1863
adithyavis <adithyaviswam@gmail.com>
2026-06-13 11:42:23 +0530

    feat: layer component

---
+++ src/components/Layer.tsx
@@ -0,0 +1,10 @@
+import { memo } from 'react';
+import type { NodeConfig } from '../core/types';
+import { Container } from './internal/Container';
+
+export type LayerProps = NodeConfig;
+
+export const Layer = memo(({ children, ...config }: LayerProps) => {
+  return <Container config={config}>{children}</Container>;
+});
+Layer.displayName = 'Layer';

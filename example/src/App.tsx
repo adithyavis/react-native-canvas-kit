074f37395cd11a5b78071fa6a63a9480c1f71cbc
adithyavis <adithyaviswam@gmail.com>
2026-07-02 15:09:41 +0530

    feat: remove prettier ignore statements

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -49,24 +49,150 @@ interface Sticker {
 }
 
 const STICKERS: Sticker[] = [
-  { id: 'gem', src: require('./assets/stickers/gem.png'), fx: 0.14, fy: 0.08, scale: 1.2, rotation: -14 }, // prettier-ignore
-  { id: 'crystal-ball', src: require('./assets/stickers/crystal-ball.png'), fx: 0.42, fy: 0.06, scale: 0.95, rotation: 16 }, // prettier-ignore
-  { id: 'star', src: require('./assets/stickers/star.png'), fx: 0.066, fy: 0.1, scale: 0.8, rotation: 8 }, // prettier-ignore
-  { id: 'unicorn', src: require('./assets/stickers/unicorn.png'), fx: 0.85, fy: 0.14, scale: 1.3, rotation: -10 }, // prettier-ignore
-  { id: 'alien', src: require('./assets/stickers/alien.png'), fx: 0.1, fy: 0.26, scale: 1.1, rotation: 22 }, // prettier-ignore
-  { id: 'robot', src: require('./assets/stickers/robot.png'), fx: 0.36, fy: 0.24, scale: 1.0, rotation: -20 }, // prettier-ignore
-  { id: 'cat-love', src: require('./assets/stickers/cat-love.png'), fx: 0.6, fy: 0.28, scale: 1.25, rotation: 12 }, // prettier-ignore
-  { id: 'rocket', src: require('./assets/stickers/rocket.png'), fx: 0.086, fy: 0.32, scale: 1.1, rotation: 30 }, // prettier-ignore
-  { id: 'sparkles', src: require('./assets/stickers/sparkles.png'), fx: 0.16, fy: 0.44, scale: 0.9, rotation: -6 }, // prettier-ignore
-  { id: 'ring', src: require('./assets/stickers/ring.png'), fx: 0.42, fy: 0.42, scale: 1.05, rotation: 18 }, // prettier-ignore
-  { id: 'crown', src: require('./assets/stickers/crown.png'), fx: 0.66, fy: 0.46, scale: 1.15, rotation: -12 }, // prettier-ignore
-  { id: 'fire', src: require('./assets/stickers/fire.png'), fx: 0.25, fy: 0.75, scale: 1.0, rotation: 10 }, // prettier-ignore
-  { id: 'rainbow', src: require('./assets/stickers/rainbow.png'), fx: 0.012, fy: 0.62, scale: 1.1, rotation: -18 }, // prettier-ignore
-  { id: 'fox', src: require('./assets/stickers/fox.png'), fx: 0.38, fy: 0.6, scale: 1.2, rotation: 14 }, // prettier-ignore
-  { id: 'panda', src: require('./assets/stickers/panda.png'), fx: 0.63, fy: 0.84, scale: 1.25, rotation: -8 }, // prettier-ignore
-  { id: 'ghost', src: require('./assets/stickers/ghost.png'), fx: 0.85, fy: 0.66, scale: 1.0, rotation: 20 }, // prettier-ignore
-  { id: 'dragon', src: require('./assets/stickers/dragon.png'), fx: 0.35, fy: 0.9, scale: 1.3, rotation: -22 }, // prettier-ignore
-  { id: 'balloon', src: require('./assets/stickers/balloon.png'), fx: 0.75, fy: 0.19, scale: 1.1, rotation: 24 }, // prettier-ignore
+  {
+    id: 'gem',
+    src: require('./assets/stickers/gem.png'),
+    fx: 0.14,
+    fy: 0.08,
+    scale: 1.2,
+    rotation: -14,
+  },
+  {
+    id: 'crystal-ball',
+    src: require('./assets/stickers/crystal-ball.png'),
+    fx: 0.42,
+    fy: 0.06,
+    scale: 0.95,
+    rotation: 16,
+  },
+  {
+    id: 'star',
+    src: require('./assets/stickers/star.png'),
+    fx: 0.066,
+    fy: 0.1,
+    scale: 0.8,
+    rotation: 8,
+  },
+  {
+    id: 'unicorn',
+    src: require('./assets/stickers/unicorn.png'),
+    fx: 0.85,
+    fy: 0.14,
+    scale: 1.3,
+    rotation: -10,
+  },
+  {
+    id: 'alien',
+    src: require('./assets/stickers/alien.png'),
+    fx: 0.1,
+    fy: 0.26,
+    scale: 1.1,
+    rotation: 22,
+  },
+  {
+    id: 'robot',
+    src: require('./assets/stickers/robot.png'),
+    fx: 0.36,
+    fy: 0.24,
+    scale: 1.0,
+    rotation: -20,
+  },
+  {
+    id: 'cat-love',
+    src: require('./assets/stickers/cat-love.png'),
+    fx: 0.6,
+    fy: 0.28,
+    scale: 1.25,
+    rotation: 12,
+  },
+  {
+    id: 'rocket',
+    src: require('./assets/stickers/rocket.png'),
+    fx: 0.086,
+    fy: 0.32,
+    scale: 1.1,
+    rotation: 30,
+  },
+  {
+    id: 'sparkles',
+    src: require('./assets/stickers/sparkles.png'),
+    fx: 0.16,
+    fy: 0.44,
+    scale: 0.9,
+    rotation: -6,
+  },
+  {
+    id: 'ring',
+    src: require('./assets/stickers/ring.png'),
+    fx: 0.42,
+    fy: 0.42,
+    scale: 1.05,
+    rotation: 18,
+  },
+  {
+    id: 'crown',
+    src: require('./assets/stickers/crown.png'),
+    fx: 0.66,
+    fy: 0.46,
+    scale: 1.15,
+    rotation: -12,
+  },
+  {
+    id: 'fire',
+    src: require('./assets/stickers/fire.png'),
+    fx: 0.25,
+    fy: 0.75,
+    scale: 1.0,
+    rotation: 10,
+  },
+  {
+    id: 'rainbow',
+    src: require('./assets/stickers/rainbow.png'),
+    fx: 0.012,
+    fy: 0.62,
+    scale: 1.1,
+    rotation: -18,
+  },
+  {
+    id: 'fox',
+    src: require('./assets/stickers/fox.png'),
+    fx: 0.38,
+    fy: 0.6,
+    scale: 1.2,
+    rotation: 14,
+  },
+  {
+    id: 'panda',
+    src: require('./assets/stickers/panda.png'),
+    fx: 0.63,
+    fy: 0.84,
+    scale: 1.25,
+    rotation: -8,
+  },
+  {
+    id: 'ghost',
+    src: require('./assets/stickers/ghost.png'),
+    fx: 0.85,
+    fy: 0.66,
+    scale: 1.0,
+    rotation: 20,
+  },
+  {
+    id: 'dragon',
+    src: require('./assets/stickers/dragon.png'),
+    fx: 0.35,
+    fy: 0.9,
+    scale: 1.3,
+    rotation: -22,
+  },
+  {
+    id: 'balloon',
+    src: require('./assets/stickers/balloon.png'),
+    fx: 0.75,
+    fy: 0.19,
+    scale: 1.1,
+    rotation: 24,
+  },
 ];
 
 const sel = (id: string) => `#${id}`;

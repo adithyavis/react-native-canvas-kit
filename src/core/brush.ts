c372f30dfc4d9491459742e9fa3c04b6e740bb10
adithyavis <adithyaviswam@gmail.com>
2026-07-04 10:21:34 +0530

    feat: spline smoothening based on points interdistance

--- src/core/brush.ts
+++ src/core/brush.ts
@@ -22,12 +22,16 @@ export function buildBrushPath(points: number[], tension = 0.5): SkPath {
     return path;
   }
 
+  const EPS = 1e-4;
+  const ALPHA_HALF = 0.25;
+  const k = (t * 2) / 3;
   const lastIndex = pointCount - 1;
+
   path.moveTo(points[0]!, points[1]!);
   for (let i = 0; i < lastIndex; i++) {
     const i0 = i - 1 < 0 ? 0 : i - 1;
     const i1 = i;
-    const i2 = i + 1 > lastIndex ? lastIndex : i + 1;
+    const i2 = i + 1;
     const i3 = i + 2 > lastIndex ? lastIndex : i + 2;
 
     const x0 = points[i0 * 2]!;
@@ -39,10 +43,31 @@ export function buildBrushPath(points: number[], tension = 0.5): SkPath {
     const x3 = points[i3 * 2]!;
     const y3 = points[i3 * 2 + 1]!;
 
-    const control1x = x1 + ((x2 - x0) * t) / 6;
-    const control1y = y1 + ((y2 - y0) * t) / 6;
-    const control2x = x2 - ((x3 - x1) * t) / 6;
-    const control2y = y2 - ((y3 - y1) * t) / 6;
+    let d1 = Math.pow(
+      (x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0),
+      ALPHA_HALF
+    );
+    let d2 = Math.pow(
+      (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1),
+      ALPHA_HALF
+    );
+    let d3 = Math.pow(
+      (x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2),
+      ALPHA_HALF
+    );
+    if (d1 < EPS) d1 = EPS;
+    if (d2 < EPS) d2 = EPS;
+    if (d3 < EPS) d3 = EPS;
+
+    const m1x = x2 - x1 + d2 * ((x1 - x0) / d1 - (x2 - x0) / (d1 + d2));
+    const m1y = y2 - y1 + d2 * ((y1 - y0) / d1 - (y2 - y0) / (d1 + d2));
+    const m2x = x2 - x1 + d2 * ((x3 - x2) / d3 - (x3 - x1) / (d2 + d3));
+    const m2y = y2 - y1 + d2 * ((y3 - y2) / d3 - (y3 - y1) / (d2 + d3));
+
+    const control1x = x1 + m1x * k;
+    const control1y = y1 + m1y * k;
+    const control2x = x2 - m2x * k;
+    const control2y = y2 - m2y * k;
 
     path.cubicTo(control1x, control1y, control2x, control2y, x2, y2);
   }
